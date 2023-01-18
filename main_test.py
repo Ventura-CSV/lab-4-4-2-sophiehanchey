8aa7aed9ced18ab425ed9243b899b75d0d5ba28c
Kyu Lee <kwooler@gmail.com>
2023-01-18 09:27:24 -0800

    L44-2

--- main_test.py
+++ main_test.py
@@ -7,7 +7,7 @@ import re
 def test_main_1():
     captureOut = io.StringIO()
     sys.stdout = captureOut
-    datastr = '150\n200\n30'
+    datastr = 'A\nB\n30'
     sys.stdin = io.StringIO(datastr)
 
     main.main()
@@ -19,6 +19,26 @@ def test_main_1():
     # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
     # res = re.search(regex_string, lines[0])
     regex_string = r'[\w,\W]*30[\w,\W]*'
+    res = re.search(regex_string, lines[0])
+    assert res != None
+    print(res.group())
+
+
+def test_main_2():
+    captureOut = io.StringIO()
+    sys.stdout = captureOut
+    datastr = 'Python\nProgram\n100'
+    sys.stdin = io.StringIO(datastr)
+
+    main.main()
+    sys.stdout = sys.__stdout__
+    print('Captured ', captureOut.getvalue())
+    lines = captureOut.getvalue().split('\n')
+    print(lines)
+
+    # regex_string = r'[\w,\W]*' + str(minval) + r'[\w,\W]*'
+    # res = re.search(regex_string, lines[0])
+    regex_string = r'[\w,\W]*100[\w,\W]*'
     res = re.search(regex_string, lines[0])
     assert res != None
     print(res.group())
